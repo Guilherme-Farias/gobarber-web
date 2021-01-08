74f3b82fc7a7cb69cccb11137553260bca12ef84
Guilherme Farias <guicfarias11@gmail.com>
2021-01-08 13:25:41 -0300

    Exibindo erros no Input

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,19 +1,28 @@
-import React, { useCallback } from 'react';
-import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import React, { useCallback, useRef } from 'react';
+import { FiArrowLeft, FiLock, FiMail, FiUser } from 'react-icons/fi';
+
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
+
 import * as Yup from 'yup';
+import getValidationsErrors from '../../utils/getValidationsErrors';
+
+import { Background, Container, Content } from './styles';
 import logoImg from '../../assets/logo.svg';
-import { Container, Content, Background } from './styles';
-import Input from '../../components/Input';
+
 import Button from '../../components/Button';
+import Input from '../../components/Input';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
   const handleSubmit = useCallback(async (data: any) => {
     try {
+      formRef.current?.setErrors({});
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
         email: Yup.string()
-          .required('Email obrigatório')
+          .required('E-mail obrigatório')
           .email('Digite um e-mail válido'),
         password: Yup.string().min(6, 'No mínimo 6 digitos'),
       });
@@ -21,9 +30,9 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
     } catch (err) {
-      console.log(err);
+      const errors = getValidationsErrors(err);
+      formRef.current?.setErrors(errors);
     }
-    console.log(data);
   }, []);
 
   return (
@@ -31,7 +40,7 @@ const SignUp: React.FC = () => {
       <Background />
       <Content>
         <img src={logoImg} alt="GoBarber" />
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu Cadastro</h1>
           <Input icon={FiUser} name="name" placeholder="Nome" />
           <Input icon={FiMail} name="email" placeholder="E-mail" />
