37a47ece76d7ce978d70bb1ea1e5df617ae7be39
Guilherme Farias <guicfarias11@gmail.com>
2021-01-13 15:55:16 -0300

    Corrigindo bug na validação + Criando timeout no Toast

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -29,7 +29,6 @@ const SignIn: React.FC = () => {
       try {
         formRef.current?.setErrors({});
         const schema = Yup.object().shape({
-          name: Yup.string().required('Nome obrigatório'),
           email: Yup.string()
             .required('E-mail obrigatório')
             .email('Digite um e-mail válido'),
